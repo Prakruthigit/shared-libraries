a518f2687f6302491cb1a9fa868824c6f1bf5bb1
Prakruthi <plearninggit@gmail.com>
2023-09-13 07:32:07 +0000

    added groovy

--- vars/lambda1.groovy
+++ vars/lambda1.groovy
@@ -1,1 +1,32 @@
-ttsvvsvvsv`
+def runPipeline(environmentName, lambdaName)
+{
+   if ("$environmentName" == 'dev' )
+    {
+            sh "mv my_deployment.zip ${environmentName}_deployment.zip"
+            withCredentials([string(credentialsId: 'access_key', variable: 'ACCESS_KEY'), string(credentialsId: 'secret_key', variable: 'SECRET_KEY')]) {
+				sh "aws lambda update-function-code --function-name ${lambdaName} --zip-file fileb://./${environmentName}_deployment.zip"
+			}
+    }
+	if ("$environmentName" == 'qa' )
+    {
+            sh "mv my_deployment.zip ${environmentName}_deployment.zip"
+            withCredentials([string(credentialsId: 'access_key', variable: 'ACCESS_KEY'), string(credentialsId: 'secret_key', variable: 'SECRET_KEY')]) {
+				sh "aws lambda update-function-code --function-name ${lambdaName} --zip-file fileb://./${environmentName}_deployment.zip"
+			}
+    }
+	if ("$environmentName" == 'staging' )
+    {
+            sh "mv my_deployment.zip ${environmentName}_deployment.zip"
+            withCredentials([string(credentialsId: 'access_key', variable: 'ACCESS_KEY'), string(credentialsId: 'secret_key', variable: 'SECRET_KEY')]) {
+				sh "aws lambda update-function-code --function-name ${lambdaName} --zip-file fileb://./${environmentName}_deployment.zip"
+			}
+    }
+	if ("$environmentName" == 'master' )
+    {
+            sh "mv my_deployment.zip ${environmentName}_deployment.zip"
+            withCredentials([string(credentialsId: 'access_key', variable: 'ACCESS_KEY'), string(credentialsId: 'secret_key', variable: 'SECRET_KEY')]) {
+				sh "aws lambda update-function-code --function-name ${lambdaName} --zip-file fileb://./${environmentName}_deployment.zip"
+			}
+    }
+}
+
